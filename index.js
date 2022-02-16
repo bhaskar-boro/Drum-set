32ece0583f750005391294d9c009beb5895650c8
Bhaskarananda Boro <97618994+bhaskar-boro@users.noreply.github.com>
2022-02-16 21:32:01 +0530

    Add initial drum kit files

---
+++ index.js
@@ -0,0 +1,48 @@
+
+document.querySelectorAll(".drum").forEach(elem=>elem.addEventListener("click", function() {
+  var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
+}));
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  var audio;
+  switch (key) {
+    case "w":
+      audio = new Audio("sounds/tom-1.mp3");
+      break;
+    case "a":
+      audio = new Audio("sounds/tom-2.mp3");
+      break;
+    case "s":
+      audio = new Audio("sounds/tom-3.mp3");
+      break;
+    case "d":
+      audio = new Audio("sounds/tom-4.mp3");
+      break;
+    case "j":
+      audio = new Audio("sounds/snare.mp3");
+      break;
+    case "k":
+      audio = new Audio("sounds/crash.mp3");
+      break;
+    case "l":
+      audio = new Audio("sounds/kick-bass.mp3");
+      break;
+    default: console.log(buttonInnerHTML);
+  }
+  audio.play();
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
